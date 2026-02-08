749344c5c09658065a4d6d4251efa3d97b2f7e5e
ammarnashirudin <ammarnashirudin14@gmail.com>
2026-02-08 21:33:33 +0700

    resolve merge conflict (restore core auth files)

---
+++ src/routers/user.router.ts
@@ -0,0 +1,14 @@
+import { Router } from "express";
+import { userController } from "../controllers/user.controllers";
+import { authMiddleware } from "../middlewares/auth.middleware";
+import { upload } from "../middlewares/uploud.middleware";
+import { authGuard } from "../middlewares/authGuard";
+
+const userRouter = Router();
+
+userRouter.get("/profile", authMiddleware,authGuard, userController.profile);
+userRouter.patch("/profile", authMiddleware,authGuard, upload.single("profileImage"), userController.updateProfile);
+userRouter.patch("/email", authMiddleware,authGuard, userController.updateEmail);
+userRouter.patch("/password", authMiddleware,authGuard, userController.updatePassword);
+
+export default userRouter;
